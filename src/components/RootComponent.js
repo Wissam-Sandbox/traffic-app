62fac756c5e7b1c31e0e1678c7015766aefbfc2e
Wissam-Sandbox <wissam.perso@gmail.com>
2018-02-06 09:20:04 +0100

    Added barebones result list and result item components.

--- src/components/RootComponent.js
+++ src/components/RootComponent.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import FilterForm from './FilterForm';
+import ItemList from './ItemList';
 
 const RootComponent = () => (
   <div>
     <div>Traffic App</div>
     <FilterForm isDisabled={false} />
+    <ItemList/>
   </div>
 );
 
